e8cc77590fc0f6fafb8daf660c0dc9a68ed31445
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-19 23:17:10 +0530

    refactor

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,33 @@
-import './App.css'
-
 function App() {
   return (
     <div>
-      <h1>Hello</h1>
+      <div className='dropdown'>
+        <button
+          className='btn btn-primary dropdown-toggle'
+          type='button'
+          data-bs-toggle='dropdown'
+          aria-expanded='false'
+        >
+          Dropdown
+        </button>
+        <ul className='dropdown-menu'>
+          <li>
+            <button className='dropdown-item' type='button'>
+              Dropdown item
+            </button>
+          </li>
+          <li>
+            <button className='dropdown-item' type='button'>
+              Dropdown item
+            </button>
+          </li>
+          <li>
+            <button className='dropdown-item' type='button'>
+              Dropdown item
+            </button>
+          </li>
+        </ul>
+      </div>
     </div>
   )
 }
