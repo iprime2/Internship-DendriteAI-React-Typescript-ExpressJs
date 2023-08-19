38c40c88b26219220a2c12c209d738f0f858753a
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-19 22:42:12 +0530

    refactor

--- api/app.tsx
+++ api/app.tsx
@@ -1,27 +1,27 @@
-import express from 'express'
+const express = require('express')
 const app = express()
-import cors from 'cors'
+const cors = require('cors')
 
 require('dotenv').config()
 
-// // db connection
-// const connectDB = require('./db/dbConnect')
+// db connection
+const connectDB = require('./db/dbConnect')
 
-// const PORT = process.env.PORT || 1010
+const PORT = process.env.PORT || 1010
 
-// app.use(cors())
+app.use(cors())
 
-// const start = async () => {
-//   try {
-//     const db = await connectDB(process.env.MONGO_URI)
-//     // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
-//     console.log('DB Connected')
-//     app.listen(PORT, () => {
-//       console.log('Server running on : ' + process.env.PORT)
-//     })
-//   } catch (error) {
-//     console.log('SERVER_OR_DB_ERROR' + error)
-//   }
-// }
+const start = async () => {
+  try {
+    const db = await connectDB(process.env.MONGO_URI)
+    // db.on('error', console.error.bind(console, 'MongoDB connection error:'))
+    console.log('DB Connected')
+    app.listen(PORT, () => {
+      console.log('Server running on : ' + process.env.PORT)
+    })
+  } catch (error) {
+    console.log('SERVER_OR_DB_ERROR' + error)
+  }
+}
 
-// start()
+start()
