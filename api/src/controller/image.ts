027121db5e74cb3727461f8fe37dc2782d90bd74
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-22 00:04:03 +0530

    fix classify api

--- api/src/controller/image.ts
+++ api/src/controller/image.ts
@@ -1,4 +1,5 @@
 import express from 'express'
+import * as mobilenet from '@tensorflow-models/mobilenet'
 
 export const imageGenerate = async (
   req: express.Request,
@@ -15,6 +16,16 @@ export const imageGenerate = async (
 
 export const classify = async (req: express.Request, res: express.Response) => {
   try {
+    const model = await mobilenet.load()
+    //@ts-ignore
+    const { image } = req
+
+    console.log(image)
+
+    const result = await model.classify(image)
+
+    console.log(result)
+    res.status(200).json(result)
   } catch (error) {
     console.error(error)
     res.status(500).json({ error: 'Internal server error' })
--- api/src/routes/image.ts
+++ api/src/routes/image.ts
@@ -1,12 +1,9 @@
 import express from 'express'
-import multer from 'multer'
 
 import { imageGenerate, classify } from '../controller/image'
 
 export default (router: express.Router) => {
   // Set up multer for handling file uploads
-  const storage = multer.memoryStorage()
-  const upload = multer({ storage })
   router.post('/image', imageGenerate)
-  router.post('/classify', upload.single('image'), classify)
+  router.post('/classify', classify)
 }
