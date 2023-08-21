c0108b0a4fcfb60786bb60c5302360b7755a9c05
Sushil Gupta <kalwarsushil2@gmail.com>
2023-08-21 22:46:04 +0530

    code refactor client

--- client/src/components/ImageUpload/ImageUpload.tsx
+++ client/src/components/ImageUpload/ImageUpload.tsx
@@ -1,10 +1,19 @@
 import React, { ChangeEvent, FC, useState } from 'react'
+import axios from 'axios'
+
 import UploadedImage from '../UploadedImage/UploadedImage'
 
 interface ImageUploadProps {}
 
+interface Prediction {
+  className: string
+  probability: number
+}
+
 const ImageUpload: FC<ImageUploadProps> = ({}) => {
   const [uploadedImage, setUploadedImage] = useState<string | null>(null)
+  const [predictions, setPredictions] = useState<Prediction[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
 
   const handleImageUpload = (event: ChangeEvent<HTMLInputElement>) => {
     const file = event.target.files?.[0]
@@ -17,6 +26,41 @@ const ImageUpload: FC<ImageUploadProps> = ({}) => {
     }
   }
 
+  const onGenerate = async () => {
+    setLoading(true)
+    const formData = new FormData()
+    if (uploadedImage) {
+      const convertedImage = await fetch(uploadedImage)
+      const blobImage = await convertedImage.blob()
+      formData.append('image', blobImage, 'image.png')
+    }
+    try {
+      const response = await axios.post(
+        'http://localhost:7070/api/classify',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      )
+      console.log(response)
+
+      if (response.data && response.data.path) {
+        const imagePath = response.data.path
+        // Continue with the rest of your code that uses the imagePath
+        setPredictions(response.data)
+      } else {
+        // Handle the case where the path property is not available
+        console.log('No path found!')
+      }
+    } catch (error) {
+      console.log(error)
+    } finally {
+      setLoading(false)
+    }
+  }
+
   return (
     <div className='w-75'>
       <div className='input-group'>
@@ -26,12 +70,15 @@ const ImageUpload: FC<ImageUploadProps> = ({}) => {
           id='inputGroupFile04'
           aria-describedby='inputGroupFileAddon04'
           aria-label='Upload'
+          disabled={loading}
           onChange={handleImageUpload}
         />
         <button
           className='btn btn-primary'
           type='button'
           id='inputGroupFileAddon04'
+          onClick={onGenerate}
+          disabled={loading}
         >
           Generate
         </button>
@@ -41,6 +88,15 @@ const ImageUpload: FC<ImageUploadProps> = ({}) => {
         <>
           <UploadedImage uploadedImage={uploadedImage} />
         </>
+      )}
+      {predictions.length > 0 && (
+        <ul>
+          {predictions.map((prediction, index) => (
+            <li key={index}>{`${prediction?.className} (${Math.round(
+              prediction.probability * 100
+            )}%)`}</li>
+          ))}
+        </ul>
       )}
     </div>
   )
